c164e59fc9c8bb047e186e678acd169185082a47
Ahmed Nahzan <nahzaaan@gmail.com>
2017-11-23 01:10:50 +0100

    Defined Test cases

--- CoffeeMaker/src/test/CoffeeMakerTest.java
+++ CoffeeMaker/src/test/CoffeeMakerTest.java
@@ -1,21 +1,61 @@
 package test;
 
+import exceptions.NotEnoughBeansException;
+import exceptions.TooManyBeansException;
+import exceptions.WaterException;
+import model.CoffeeMaker;
 import org.junit.Before;
 import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
 
 
 public class CoffeeMakerTest {
 
+    private CoffeeMaker cmaker = new CoffeeMaker();
+
     @Before
-    public void setUp(){
-        // TODO: instantiate your test objects here
+    public void initialize(){
+        CoffeeMaker cmaker = new CoffeeMaker();
     }
 
     @Test
-    public void testTemplate(){
-        // TODO: write some more tests
-        // This is a template for unit tests
+    public void testBrew(){
+        // Testing whether brew works under normal conditions
+        try {
+            cmaker.brew(2.5,15);
+        } catch (NotEnoughBeansException e) {
+            fail("Not enough beans");
+        } catch (TooManyBeansException e) {
+            fail("Too many beans");
+        } catch (WaterException e) {
+            fail("Not enough water");
+        }
     }
+
+    @Test
+    public void testTimeSetter() {
+        // Testing whether brew works under normal conditions
+        try {
+            cmaker.setTimeSinceLastBrew(14);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void pourCoffee() {
+        // Testing whether brew works under normal conditions
+        try {
+            cmaker.brew(2.5,15);
+            cmaker.setTimeSinceLastBrew(15);
+            cmaker.pourCoffee();
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+
 
 
 }
