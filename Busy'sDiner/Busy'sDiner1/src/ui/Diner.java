c4cced10365f06e8c2e12b3fd1a753005bee094c
Ahmed Nahzan <nahzaaan@gmail.com>
2017-12-09 17:35:31 +0100

    Divided Employee to two classes, Chef and Server.

--- Busy'sDiner/Busy'sDiner1/src/ui/Diner.java
+++ Busy'sDiner/Busy'sDiner1/src/ui/Diner.java
@@ -1,33 +1,35 @@
 package ui;
 
-import model.Employee;
+import model.Server;
 import model.Order;
+import model.Chef;
 
 public class Diner {
 
     public static void main(String[] args) {
-        Employee employee = new Employee();
+        Server server = new Server();
+        Chef chef = new Chef();
 
         for (int i=0; i < 2 ; i++) {
             System.out.println("Table " + (i + 1) + ":\n");
 
-            employee.greet();
-            employee.describeDish();
-            Order o = employee.takeOrder();
+            server.greet();
+            server.describeDish();
+            Order o = server.takeOrder();
 
             System.out.println();
 
-            doOrderRoutine(employee, o);
+            doOrderRoutine(chef, server, o);
             System.out.println();
         }
 
         System.out.println();
-        employee.doDishes();
+        chef.doDishes();
     }
 
-    private static void doOrderRoutine(Employee e, Order o) {
+    private static void doOrderRoutine(Chef chef, Server e, Order o) {
         if (o.needsToBePrepared())
-            e.makeDish(o);
+            chef.makeDish(o);
         System.out.println();
         if (o.isReadyToBeServed())
             e.deliverFood(o);
